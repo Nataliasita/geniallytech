8eb8092e53ab6282517318a087c65e7007124810
Luz Angela Martinez <luzangela.lic@gmail.com>
2022-07-31 09:15:29 -0500

    cambios de imagenes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,7 +11,7 @@ public arrayImg: any[] = [];
 
   ngOnInit(): void {
     this.arrayImg=[
-      {link:"https://img.freepik.com/foto-gratis/diversos-ninos-leyendo-libros_53876-146406.jpg?w=740&t=st=1659236602~exp=1659237202~hmac=51a2983e70ebdc302eb39a42a6abe4bd705bd30fc9a0cc85af7f5a89b63b259d", principal: true, nombre:"emeplo01" },
+      {link:"../../assets/images/niños.png", principal: true, nombre:"emeplo01" },
       {link:"https://img.freepik.com/foto-gratis/iconos-espacio-exterior-dibujo-concepto-graficos_53876-167098.jpg?w=740&t=st=1659236631~exp=1659237231~hmac=9bf610f4dd0b3258656f82e8d32dcb23907a5f1ffdfac2f6494f92f21274e0b4", principal: false, nombre:"emeplo02" },
       {link:"https://img.freepik.com/foto-gratis/ninos-divirtiendose-juegos-tradicionales_23-2149317168.jpg?w=826&t=st=1659236687~exp=1659237287~hmac=bf4bc1846effcabada7515e896b7d6574ed4b89ec04577e024d69293dafa74e9", principal: false, nombre:"emeplo03" }
     ]
